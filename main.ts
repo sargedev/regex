37e413c7d91f5f80d66001e507c2a60a1439505f
Sarge <63020240+sargedev@users.noreply.github.com>
2024-06-06 20:26:01 +0200

    Added Match object

--- main.ts
+++ main.ts
@@ -1,1 +1,14 @@
 
+namespace regex {
+    class Match {
+        start: number;
+        end: number;
+        text: string;
+
+        constructor(text: string, start: number, end: number) {
+            this.text = text;
+            this.start = start;
+            this.end = end;
+        }
+    }
+}
