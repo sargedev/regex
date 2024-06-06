9199b7205499e54f2dabef54337e21a1d16f037f
Sarge <63020240+sargedev@users.noreply.github.com>
2024-06-06 20:58:43 +0200

    Updated tokens

--- main.ts
+++ main.ts
@@ -1,14 +1,22 @@
 
 namespace regex {
 
-    const TT_START = "START";
-    const TT_END = "END";
+    const TT_DOLLAR = "START";
+    const TT_CARROT = "END";
     const TT_LPAREN = "LPAREN";
     const TT_RPAREN = "RPAREN";
     const TT_LSPAREN = "LSPAREN";
     const TT_RSPAREN = "RSPAREN";
     const TT_LCPAREN = "LCPAREN";
     const TT_RCPAREN = "RCPAREN";
+    const TT_PERIOD = "PERIOD";
+    const TT_PIPE = "PIPE";
+    const TT_BSLASH = "BSLASH";
+    const TT_DASH = "DASH";
+    const TT_ASTERISK = "ASTERISK";
+    const TT_QUESTION = "QUESTION";
+    const TT_PLUS = "PLUS";
+    const TT_COMMA = "COMMA";
 
     class Token {
         type: string;
@@ -67,14 +75,22 @@ namespace regex {
 
             while (this.char !== null) {
                 switch (this.char) {
-                    case "^": tokens.push(new Token(TT_START));
-                    case "$": tokens.push(new Token(TT_END));
-                    case "(": tokens.push(new Token(TT_END));
-                    case ")": tokens.push(new Token(TT_END));
-                    case "[": tokens.push(new Token(TT_END));
-                    case "]": tokens.push(new Token(TT_END));
-                    case "{": tokens.push(new Token(TT_END));
-                    case "}": tokens.push(new Token(TT_END));
+                    case "^": tokens.push(new Token(TT_DOLLAR));
+                    case "$": tokens.push(new Token(TT_CARROT));
+                    case "(": tokens.push(new Token(TT_CARROT));
+                    case ")": tokens.push(new Token(TT_CARROT));
+                    case "[": tokens.push(new Token(TT_CARROT));
+                    case "]": tokens.push(new Token(TT_CARROT));
+                    case "{": tokens.push(new Token(TT_CARROT));
+                    case "}": tokens.push(new Token(TT_CARROT));
+                    case ".": tokens.push(new Token(TT_PERIOD));
+                    case "|": tokens.push(new Token(TT_PIPE));
+                    case "\\": tokens.push(new Token(TT_BSLASH));
+                    case "-": tokens.push(new Token(TT_DASH));
+                    case "*": tokens.push(new Token(TT_ASTERISK));
+                    case "?": tokens.push(new Token(TT_QUESTION));
+                    case "+": tokens.push(new Token(TT_PLUS));
+                    case ",": tokens.push(new Token(TT_COMMA));
                 }
             }
             return tokens;
